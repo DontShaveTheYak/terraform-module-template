ce7750b71cb0f9e7db75ba50030c7a47ad40245d
Levi Blaney <shadycuz@gmail.com>
2022-07-09 02:36:36 +0000

    remove one function as it was only added in tf 0.15

--- modules/greet_multiple/main.tf
+++ modules/greet_multiple/main.tf
@@ -2,7 +2,7 @@ locals {
   len      = length(var.names)
   csv_list = slice(var.names, 0, local.len - 1)
   and_list = slice(var.names, local.len - 1, local.len)
-  input    = "${join(", ", local.csv_list)} and ${one(local.and_list)}"
+  input    = "${join(", ", local.csv_list)} and ${local.and_list[0]}"
 }
 
 module "hello" {
